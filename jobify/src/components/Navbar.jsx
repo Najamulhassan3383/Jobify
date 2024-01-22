f9af9dde32421bbf774146864d3c1bf4699a7f9a
Najamulhassan3383 <najmulhassan721@gmail.com>
2024-01-22 13:55:32 +0500

    completed navbar

--- jobify/src/components/Navbar.jsx
+++ jobify/src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar.js";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo.jsx";
+import { useDashboardContext } from "../pages/DashboardLayout.jsx";
 function Navbar() {
-  return <Wrapper>Navbar</Wrapper>;
+  const { toggleSideBar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSideBar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">tooggle/logout</div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Navbar;
